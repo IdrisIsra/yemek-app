c0402e316b2e77d59ff28a00d6613c2c23474797
IdrisIsra <israpilov.idris@gmail.com>
2019-01-06 02:34:18 +0300

    nice button added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
 import Stars from './Stars.js'
-import logo from './logov1.png'
+import logo from './logov2.png'
+import { Button } from 'react-bootstrap';
+
 
 
 class App extends Component {
+
+
   constructor() {
     super();
 
     this.state = {
-      rating: 1
+      rating: 1,
+      voted: false
     };
     this.onStarClick = this.onStarClick.bind(this);
+    this.submitRating = this.submitRating.bind(this);
   }
 
   onStarClick(nextValue, prevValue, name) {
     this.setState({rating: nextValue});
   }
+  submitRating() {
+    this.setState({voted: true})
+  }
   render() {
     const rating = this.state.rating;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React idrisv2
-          </a>
-          <Stars value={rating} onClick={this.onStarClick}/>
-        </header>
+    const voted = this.state.voted;
+    if (voted === true) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Thanks for voting!
+            </p>
+            <Stars value={rating} />
+          </header>
 
-      </div>
-    );
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Please rate today's food!
+            </p>
+            <Stars value={rating} onClick={this.onStarClick}/>
+            <Button bsStyle="primary" bsSize="large" onClick={this.submitRating} className="voteButton">
+              Vote!
+            </Button>
+          </header>
+
+        </div>
+      );
+    }
+
   }
 }
 
